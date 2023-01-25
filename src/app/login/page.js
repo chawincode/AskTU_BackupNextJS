5a0a2042226d0641e514f11fbfcd2fe89c09e267
chawincode <chawin.suw@dome.tu.ac.th>
2023-01-26 01:24:04 +0700

    config

---
+++ src/app/login/page.js
@@ -0,0 +1,12 @@
+import React from "react";
+import SideBar from "../SideBar";
+
+function Home() {
+    return (
+        <div className="column">
+            <SideBar />
+        </div>
+    )
+}
+
+export default Home;
